55aaa019441e9d9c6b9f4e6708ee780251ebf607
George Higbie <gehigbie@gmail.com>
2018-04-07 22:30:02 +0000

    Install babel dependencies

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 console.log('app js js is running!');
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import PredicateBuilderApp from './components/PredicateBuilderApp.js';
 
 const appRoot = document.getElementById('react-container');
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import PredicateBuilderApp from './PredicateBuilderApp.js';
-
-ReactDOM.render(<PredicateBuilderApp />, document.getElementById('react-container'));
