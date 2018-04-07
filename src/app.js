d2405b27f1df7c1e5eed97b6a7c30c0a2e617563
George Higbie <gehigbie@gmail.com>
2018-04-07 21:54:05 +0000

    Add basic react app

---
+++ src/app.js
@@ -0,0 +1,15 @@
+console.log('app js js is running!');
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+
+class PredicateBuilderApp extends Component{
+    render(){
+        return(
+            <div>
+                <h1>Add dogs are awesome!</h1>
+            </div>
+        );
+    }
+}
+
+export default PredicateBuilderApp;
