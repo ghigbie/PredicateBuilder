255084df7fc99eeaa89ae398dd65dbf6d7efa695
George Higbie <gehigbie@gmail.com>
2018-04-07 20:10:50 +0000

    Try es6 approacf for express server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
-const express = require('express'),
-         path = require('path');
-          app = express();
+import express from 'express';
+import path from 'path';
+
+const app = express();
           
 const publicPath = path.join(__dirname, '..', 'public');
 
