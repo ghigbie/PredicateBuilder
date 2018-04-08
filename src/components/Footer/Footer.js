8adab89792d68804f8611cb1a76430db2da752b7
George Higbie <gehigbie@gmail.com>
2018-04-08 19:57:50 +0000

    Modify bootstrap classes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,12 +3,12 @@ import './Footer.css';
 
 const Footer = () => {
     return(
-        <footer>
-            <p className="d-inline">Returned 4999 results</p>
-            <div className="d-inline float-right" >
+        <footer className="row footer">
+            <p className="d-inline grey-text col-md-6">Returned 4999 results</p>
+            <div className="d-inline float-right col-md-6">
                 <button className="btn btn-sm">Export</button>
-                <button className="btn btn-light btn-sm">Save Preset</button>
-                <button className="btn btn-primary btn-sm">Search</button>
+                <button className="btn btn-light btn-sm button-wide">Save Preset</button>
+                <button className="btn btn-primary btn-sm button-wide">Search</button>
             </div>
         </footer>
     );
