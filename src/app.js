b0a4cf749b699b3142d992e5dbbb5b1dd9bd4b48
George Higbie <gehigbie@gmail.com>
2018-04-08 21:02:39 +0000

    Change file structure

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 console.log('app js js is running!');
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PredicateBuilderApp from './components/PredicateBuilderApp.js';
+import PredicateBuilderApp from './components/PredicateBuilder/PredicateBuilderApp.js';
 
 const appRoot = document.getElementById('react-container');
 ReactDOM.render(<PredicateBuilderApp />, appRoot);
--- src/components/PredicateBuilder/PredicateBuilderApp.js
+++ src/components/PredicateBuilder/PredicateBuilderApp.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import './PredicateBuilderApp.css';
 
-import Header from './Header/Header';
-import Footer from './Footer/Footer';
+import Header from './../Header/Header';
+import Footer from './../Footer/Footer';
 
 class PredicateBuilderApp extends Component{
     render(){
         return(
-            <div className="container-fluid">
+            <div className="container-fluid main-app">
                 <Header />
                 <h1>This is a body</h1>
                 <Footer />
