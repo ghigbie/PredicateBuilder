40f3171481c3d20292d1e792519c5ec151c3e080
George Higbie <gehigbie@gmail.com>
2018-04-08 19:43:12 +0000

    Add bootstrap classes to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
     return(
-        <div>
-            <p>Returned 4999 results</p>
-            <button className="btn btn-sm">Export</button>
-            <button className="btn btn-light btn-sm">Save Preset</button>
-            <button className="btn btn-primary btn-sm">Search</button>
-        </div>
+        <footer>
+            <p className="d-inline">Returned 4999 results</p>
+            <div className="d-inline float-right" >
+                <button className="btn btn-sm">Export</button>
+                <button className="btn btn-light btn-sm">Save Preset</button>
+                <button className="btn btn-primary btn-sm">Search</button>
+            </div>
+        </footer>
     );
 };
 
