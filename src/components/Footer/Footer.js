b3c886fa51ddba9e195b97b81bd7345feda912f9
George Higbie <gehigbie@gmail.com>
2018-04-08 20:28:48 +0000

    Change button order on float right

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,11 +4,11 @@ import './Footer.css';
 const Footer = () => {
     return(
         <footer className="row footer">
-            <p className="d-inline grey-text col-md-6">Returned 4999 results</p>
-            <div className="d-inline float-right col-md-6">
-                <button className="btn btn-sm footer-button">Export</button>
-                <button className="btn btn-light btn-sm footer-button button-wide">Save Preset</button>
-                <button className="btn btn-primary btn-sm footer-button button-wide">Search</button>
+            <p className="col-md-6 d-inline grey-text">Returned 4999 results</p>
+            <div className="col-md-6 d-inline button-holder">
+                <button className="btn btn-primary btn-sm float-right footer-button button-wide">Search</button>
+                <button className="btn btn-light btn-sm float-right footer-button button-wide">Save Preset</button>
+                <button className="btn btn-sm float-right footer-button">Export</button>
             </div>
         </footer>
     );
