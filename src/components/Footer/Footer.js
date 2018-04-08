ad4e24034afe9953d1527750edfb3b2bccbda5d9
George Higbie <gehigbie@gmail.com>
2018-04-08 20:00:06 +0000

    Customize footer styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,9 @@ const Footer = () => {
         <footer className="row footer">
             <p className="d-inline grey-text col-md-6">Returned 4999 results</p>
             <div className="d-inline float-right col-md-6">
-                <button className="btn btn-sm">Export</button>
-                <button className="btn btn-light btn-sm button-wide">Save Preset</button>
-                <button className="btn btn-primary btn-sm button-wide">Search</button>
+                <button className="btn btn-sm footer-button">Export</button>
+                <button className="btn btn-light btn-sm footer-button button-wide">Save Preset</button>
+                <button className="btn btn-primary btn-sm footer-button button-wide">Search</button>
             </div>
         </footer>
     );
