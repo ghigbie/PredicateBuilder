da28951a16e6b20bc8e5d0595edadd30b35fa0e5
George Higbie <gehigbie@gmail.com>
2018-04-08 18:10:22 +0000

    Add footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+    return(
+        <div>
+            <h3>Returned 4999 results</h3>
+            <span>
+                <button>Export</button>
+                <button>Save Preset</button>
+                <button>Search</button>
+            </span>
+        </div>
+    );
+};
+
+export default Footer;
--- src/components/PredicateBuilderApp.js
+++ src/components/PredicateBuilderApp.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 
+import Footer from './Footer/Footer';
+
 class PredicateBuilderApp extends Component{
     render(){
         return(
             <div>
                 <h1>All dogs are awesome!</h1>
+                <Footer />
             </div>
         );
     }
