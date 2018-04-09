76d6eb215ddd1e3af627999074184a9ebb3e0f0d
George Higbie <gehigbie@gmail.com>
2018-04-09 14:26:03 +0000

    Add predicateFields array

--- src/components/BodySearch/BodySerach.js
+++ src/components/BodySearch/BodySerach.js
@@ -2,12 +2,18 @@ import React, { Component } from 'react';
 import './BodySearch.css';
 
 class BodySearch extends Component{
+    constructor(props){
+        super(props);
+        
+        this.predicateFields = ['User email', 'screen width', 'screen heigth', '# of visits', 'First Name', 'Last Name', 'Page Response time (ms)', 'Domain', 'Page Path'];
+    }
+    
     render(){
         return(
             <div>
                 <button>+</button>
-                <button></button>
-                <button></button>
+                <button className="btn btn-secondary dropdown-toggle"></button>
+                <button className="btn btn-secondary dropdown-toggle"></button>
                 <input type="text" placeholder="Please select" />
                 <button>And</button>
                 <input type="text" placeholder="Please select" />
