30da6b50a28f35af993ba0772d2aa66919f5230a
George Higbie <gehigbie@gmail.com>
2018-04-09 01:47:48 +0000

    Create side table item component

---
+++ src/components/SideTableItem/SideTableItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './SideTableItem.css';
+
+const SideTableItem = (props) => {
+    return(
+        <div>
+        </div>
+    );
+};
+
+export default SideTableItem;
