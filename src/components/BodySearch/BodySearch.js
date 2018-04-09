1cdc25ccdd2b562d3f15b17538c4cf15af901df4
George Higbie <gehigbie@gmail.com>
2018-04-09 15:45:38 +0000

    Modify map function

--- src/components/BodySearch/BodySearch.js
+++ src/components/BodySearch/BodySearch.js
@@ -10,16 +10,24 @@ class BodySearch extends Component{
     }
     
     predicateFieldBuilder(){
-        const predicateFields = this.predicateField.map((field, index) => <a key={index} className="dropdown-item" href="#">{field}</a>);
+       return this.predicateField.map((field, index) => <a key={index} className="dropdown-item" href="#">{field}</a>);
     }
     
     render(){
         return(
             <div>
-                <button>+</button>
-                <button className="btn btn-secondary dropdown-toggle">
-                    {this.predicateFields.map((field, index) =>  <a key={index} className="dropdown-item" href="#">{field}</a> )}
-                </button>
+                <button className="btn btn-basic">+</button>
+                
+                <div className="dropdown show d-inline">
+                  <a className="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Dropdown link
+                  </a>
+                
+                  <div clasName="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                    {this.predicateFields.map((field, index) => <a key={index} className="dropdown-item" href="#">{field}</a>)}
+                  </div>
+                </div>
+
                 <button className="btn btn-secondary dropdown-toggle"></button>
                 <input type="text" placeholder="Please select" />
                 <button>And</button>
