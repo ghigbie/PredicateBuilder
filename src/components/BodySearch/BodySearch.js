20a1a246991811d70ad464d50d78636fe99fb5ed
George Higbie <gehigbie@gmail.com>
2018-04-11 01:48:46 +0000

    Remove commented code

--- src/components/BodySearch/BodySearch.js
+++ src/components/BodySearch/BodySearch.js
@@ -17,36 +17,11 @@ class BodySearch extends Component{
         this.sqlTitle = 'SQL Search';
         this.handleAddRow = this.handleAddRow.bind(this);
         this.handleRemoveRow = this.handleRemoveRow.bind(this);
-        this.buttonRow = (<div className="d-block">
-                                <ButtonRow subjectFields={this.subjectFields}
-                                           sqlItems={this.sqlItems}
-                                           verbWords={this.verbWords}
-                                           searchTitle={this.searchTitle}
-                                           sqlTitle={this.sqlTitle}
-                                           removeRow={this.handleRemoveRow}
-                                           className="button-row"/>
-                            </div>);
-        
         this.state = {
             rows: [1],
             count: 1
-            // [{number: 0, active: true}, {number: 1, active: false}, {number: 2, active: false}, {number: 3, active: false},
-            // {number: 4, active: false}, {number: 5, active: false}, {number: 6, active: false}],
-            // rowCount: 0
         };
     }
-    
-    // createButtonRow(){
-    //     console.log('CREATE buttonrow called');
-    //     return  (<div className="d-block">
-    //                             <ButtonRow subjectFields={this.subjectFields}
-    //                                       sqlItems={this.sqlItems}
-    //                                       verbWords={this.verbWords}
-    //                                       searchTitle={this.searchTitle}
-    //                                       sqlTitle={this.sqlTitle}
-    //                                       className="button-row"/>
-    //                         </div>);
-    // }
     
     handleVisibility(){
       this.setState((prevState) => {
@@ -69,17 +44,6 @@ class BodySearch extends Component{
             rows: bools
         });
     }
-    
-    // addExtraRow(){
-    //     let index = this.setState((prevState) => {
-    //         rowCount: prevState.rowCount++;
-    //     });
-        
-        // this.setState(() => {
-        //     rows: rows[index].active = true;
-        // });
-   // }
-    
     
     render(){
         return(
