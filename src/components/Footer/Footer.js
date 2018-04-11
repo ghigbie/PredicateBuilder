17589fc7464e7eb2e0aeb725180611078b639b48
George Higbie <gehigbie@gmail.com>
2018-04-11 06:36:46 +0000

    Restyle buttons in footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,8 +7,8 @@ const Footer = () => {
             <p className="col-md-6 d-inline grey-text">Returned 4999 results</p>
             <div className="col-md-6 d-inline button-holder">
                 <button className="btn btn-primary btn-sm float-right footer-button button-wide">Search</button>
-                <button className="btn btn-light btn-sm float-right footer-button button-wide">Save Preset</button>
-                <button className="btn btn-sm float-right footer-button">Export</button>
+                <button className="btn btn-outline-secondary btn-sm float-right footer-button button-wide">Save Preset</button>
+                <button className="btn btn-secondary btn-sm float-right footer-button">Export</button>
             </div>
         </footer>
     );
