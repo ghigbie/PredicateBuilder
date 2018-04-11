61fba9098d61f2a9fe974af7bbc35f1575a83f0c
George Higbie <gehigbie@gmail.com>
2018-04-11 05:36:37 +0000

    Add SearchNumber component

--- src/components/SearchNumber/SearchNumber.js
+++ src/components/SearchNumber/SearchNumber.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 import DropdownButton from './../DropdownButton/DropdownButton';
 
-const SearchWord = () => {
+const SearchNumber= (props) => {
     return(
         <div>
+        
+            <button className="btn btn-default row-item">Is</button>
             
             <div className="d-inline row-item">
-                <DropdownButton listItems={props.verbWords}
-                                    className="d-inline row-item" />
+                <DropdownButton listItems={props.verbNumbers}
+                                className="d-inline row-item" />
             </div>
             
             <button className="btn btn-default row-item">And</button>
@@ -21,84 +23,4 @@ const SearchWord = () => {
     );
 };
 
-export default SearchWord;
-
-
-import React, { Component } from 'react';
-import './ButtonRow.css';
-
-import DropdownButton from './../DropdownButton/DropdownButton';
-
-                               
-class ButtonRow extends Component{
-    constructor(props){
-        super(props);
-        this.handleChangeToNumber = this.handleChangeToNumber.bind(this);
-        this.handleChangetoWord = this.handleChangetoWord.bind(this);
-       // this.handleChange = this.handleChange.bind(this);
-        this.state = {
-            isWord: true,
-            queryString: ''
-        };
-    }
-        
-    handleChangeToNumber(){
-        this.setState(() => ({isWord: false}));
-    }
-    
-    handleChangeToWord(){
-        this.setState(() => ({isWord: true}));
-    }
-    
-    // handleChange(value){
-    //     value.type === 'text'? this.handleChangeToWord() : this.handleChangeToNumber();
-    // }
-    
-    render(){
-        
-        const wordRender = (
-            <div>
-                <div className="d-inline row-item">
-                    <DropdownButton listItems={this.props.subjectFields}
-                                    className="d-inline row-item" /> 
-                </div>
-                <input className="form-control d-inline row-item input-item"
-                           type="text" 
-                           placeholder="Please select" />
-            </div>
-        );
-        
-        const numberRender = (
-            <div>
-            
-                <div className="d-inline row-item">
-                    <DropdownButton listItems={this.props.verbWords}
-                                    className="d-inline row-item" />
-                </div>
-            
-                <button className="btn btn-default row-item">And</button>
-                    
-                <input className="form-control d-inline row-item input-item"
-                       type="text" 
-                       placeholder="Please select" />
-            </div>
-            
-        );
-           
-        return(
-            <div className="d-inline input-group row-item">
-                    
-                <div className="d-inline row-item">
-                    <DropdownButton listItems={this.props.subjectFields}
-                                    className="d-inline row-item"  />
-                </div>
-                
-                {this.state.isWord ? wordRender : numberRender}
-
-                    
-            </div>
-        );
-    }
-}
-
-export default ButtonRow;
+export default SearchNumber;
